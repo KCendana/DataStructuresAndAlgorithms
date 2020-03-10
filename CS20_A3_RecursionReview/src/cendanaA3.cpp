29ad7607e910e30248d0c19beb00340feee67a70
Kevin Cendana <55429778+KCendana@users.noreply.github.com>
2020-03-10 07:37:44 -0700

    Got rid of self note comments

--- CS20_A3_RecursionReview/src/cendanaA3.cpp
+++ CS20_A3_RecursionReview/src/cendanaA3.cpp
@@ -6,7 +6,6 @@ int fib(int i);
 void prime(int i);
 bool primeChecker(int i, int checker = 2);
 int mult(int i, int n);
-int multHelper(int i, int n);
 string reverseString(string s);
 string reverseStringHelper(string s);
 int countOccurrences(string s, string c);
@@ -37,28 +36,6 @@ int main() {
 		cout << "number of occurrences of " << t[0] << " in " << s << " is " << countOccurrences(s,t) << endl;
 
 	    return 0;
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MY OWN TEST CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//	cout << fact(5) << endl;
-//	cout << fib(7) << endl;
-//	int i = 11;
-//	bool prime = primeChecker(i);
-//	if (prime == true)
-//	{
-//		cout << i << " is prime. " << endl;
-//	}
-//	else if (prime == false)
-//	{
-//		cout << i << " is NOT prime " << endl;
-//	}
-//	prime(20);
-//	cout << mult(5,0);
-//	string test = " ?esiw eht siugalP htraD fo ydegart eht fo draeh reve uoy evaH";	//azzip snhoj apap
-//	cout << reverseString(test);
-//	cout << test << endl;
-//	string word = "my name jeff";
-//	string search = "f";
-//	cout << countOccurrences(word, search);
 }
 
 int fact(int i)
@@ -70,16 +47,13 @@ int fact(int i)
 	}
 
 	return i*fact(i-1);
-	//4 x 3
-	//3 x 2
-	//2 x 1
-	//1 x 1
-	//^ returns first int and multiplies them all
+
 }
+
 int fib(int i)
 {
 	//base cases
-	if (i <= 0)	//i mean.... fibonocci sequence starts at 1 so i guess anything before that is 0
+	if (i <= 0)
 	{
 		return 0;
 	}
@@ -91,6 +65,7 @@ int fib(int i)
 
 	return fib(i-1) + fib(i-2);
 }
+
 void prime(int i)
 {
 	int counter = 0;
@@ -106,19 +81,18 @@ void prime(int i)
 		counter++;
 	}
 }
+
 bool primeChecker(int i, int checker)
 {
 	//base case: number is 1
 	if (i == 1)
 	{
-		//cout << "primerChecker: Number is 1, which is technically NOT prime for some reason. " << endl;
 		return false;
 	}
 
 	//base case: number is less than 2 (excluding the number 1)
 	if (i <= 2)
 	{
-		//cout << "primeChecker: Number is equal to or less than 2 (and isn't 1) so it's prime. " << endl;
 		return true;
 	}
 
@@ -127,18 +101,17 @@ bool primeChecker(int i, int checker)
 	//ex) if number = 7 and checker == 7, we have attempted to divide number by 1, 2, 3, 4, 5, and 6, with all of them having a remainder, so it's prime
 	if (checker >= i)
 	{
-		//cout << "primeChecker: Checker is now the same number as number. " << endl;
 		return true;
 	}
 
 	if (i % checker == 0)
 	{
-		//cout << "primeChecker: Number is not prime. " << endl;
 		return false;
 	}
 
 	primeChecker(i, checker+1);
 }
+
 int mult(int i, int n)
 {
 	if (n == 0)
@@ -147,10 +120,7 @@ int mult(int i, int n)
 	}
 	return mult(i, n-1) + i;
 }
-int multHelper(int i, int n)
-{
-	//i didn't need a helper :(
-}
+
 string reverseString(string s)
 {
 	if (s.length() == 1)
@@ -160,6 +130,7 @@ string reverseString(string s)
 	string newString = reverseStringHelper(s);
 	return newString;
 }
+
 string reverseStringHelper(string s)
 {
 	if (s.length() == 1)
@@ -167,9 +138,7 @@ string reverseStringHelper(string s)
 		return s.substr(0,1);
 	}
 	string firstLetter = s.substr(s.length()-1,s.length()-1);	//first letter of "ABC" is 'C'
-	//cout << "firstLetter: " << firstLetter << endl;
 	s = s.substr(0, s.length()-1);	//"ABCDEF" becomes "ABCDE"
-	//cout << "entire string is now: " << s << endl;
 	return firstLetter + reverseStringHelper(s);
 }
 int countOccurrences(string s, string c)
@@ -182,17 +151,14 @@ int countOccurrencesHelper(string s, string c, int counter, int wordSize)
 {
 	if(counter == wordSize)
 	{
-		//cout << "Counter: " << counter << endl;
 		return 0;
 	}
 	if(s.substr(counter, 1) == c)
 	{
-		//cout << c << " found. Counter: " << counter << endl;
 		return 1 + countOccurrencesHelper(s, c, counter+1, wordSize);
 	}
 	else
 	{
-		//cout << "Searching " << s.substr(counter,1) << "... " << c << " not found. Counter: " << counter << endl;
 		return countOccurrencesHelper(s, c, counter+1, wordSize);
 	}
 }
